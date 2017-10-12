96ff7b09ae1d474d932c01ff3acc37bde5492d28
Koby Allard <koby.allard@smartsheet.com>
2017-10-12 13:37:12 -0700

    bugfix: cast argument number of columns to int

--- check_layout.py
+++ check_layout.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
 import i3
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 currentWindow = i3.filter(focused=True)[0]
-
-parentContainer = i3.parent(currentWindow['id'])
-print(parentContainer['layout'])
+i3.focus(con_id=currentWindow['id'])
+pp.pprint(currentWindow)
+height = currentWindow['rect']['height']
+print(height)
+print(height - 235)
+#i3.resize('shrink height 10')
+i3.resize('grow height 1000')
